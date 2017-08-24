a721605e4fd551c9a3bd9d3e8f8a08c03efcf891
Billz <bill.zsj95@gmail.com>
2017-08-25 00:10:02 +1000

    http forwarding

--- HTTPHandler.py
+++ HTTPHandler.py
@@ -1,11 +1,55 @@
 from http.server import BaseHTTPRequestHandler as base
+import socket as sk
 import requests
 
-class HTTPHandler(base):
-	def do_GET(self):
-		print('GET')
-		return requests.get(self.path).text
+division = '''
+===========================================================
+===========================================================
+===========================================================
 
-	def do_CONNECT(self):
-		print('CONNECT')
-		return requests.get('https://'+self.path.split(':')[0]).text
+'''
+
+
+class HTTPHandler(base):
+
+    def do_GET(self):
+        print('GET')
+        clientSocket = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+
+        connectName = self.path.split(':')[1]
+        port = 80
+        slash_index = connectName.index("/", 2)
+        print("before!!", connectName, "\nAfter!!!",  connectName[2:slash_index])
+
+
+        ipAddr = sk.gethostbyname(connectName[2:slash_index])
+
+        clientSocket.connect((ipAddr, port))
+        clientSocket.send((str(self.requestline) + "\r\n" + str(self.headers)).encode())
+
+        print (self.requestline , "\r\n" , self.headers)
+
+        msg = clientSocket.recv(2048)
+
+        clientSocket.close()
+        self.wfile.write(msg)
+
+    def do_CONNECT(self):
+        print('CONNECT')
+
+        clientSocket = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+
+        connectName, port = self.path.split(':')
+        port = int(port)
+        print(connectName, port)
+
+        ipAddr = sk.gethostbyname(connectName)
+
+        clientSocket.connect((ipAddr, port))
+        clientSocket.send((str(self.requestline) + "\r\n" + str(self.headers)).encode())
+
+        print (self.requestline , "\r\n" , self.headers)
+        msg = clientSocket.recv(2048)
+
+        clientSocket.close()
+        self.wfile.write(msg)
--- aslocal.py
+++ aslocal.py
@@ -5,43 +5,47 @@ import struct
 from HTTPHandler import HTTPHandler
 import socketserver
 
+
 class Connection():
-	def __init__(self, server_address, port):
-		self.rsocket = socket.socket()
-		self.rhost = server_address
-		self.rport = port
-		try:
-			self.rsocket.connect((self.rhost, self.rport))
-		except Exception as e:
-			print(str(e))
+    def __init__(self, server_address, port):
+        self.rsocket = socket.socket()
+        self.rhost = server_address
+        self.rport = port
+        try:
+            self.rsocket.connect((self.rhost, self.rport))
+        except Exception as e:
+            print(str(e))
 
-	def authenticate(self, **kwargs):
-		nMethods = struct.unpack('b', self.rsocket.recv(1))[0]
-		methods = struct.unpack('{}b'.format(nMethods), self.rsocket.recv(nMethods))
-		print('Server Supported Authentication Methods: ', methods)
-		if not kwargs['method'] in methods:
-			raise Exception('Unsupported authentication method')
-		if kwargs['method'] == 1: #username, password authentication
-			try:
-				username = kwargs['username']
-				password = kwargs['password']
-			except:
-				print('Missing username and/or password')
-			self.rsocket.send(struct.pack('b', kwargs['method']))
-			usernameHash = hl.md5(username.encode()).digest()
-			passwordHash = hl.md5(password.encode()).digest()
-			self.rsocket.send(usernameHash+passwordHash)
-			authResponse = self.rsocket.recv(1024)
-			if authResponse == b'Authentication Successful':
-				self.listenLocal(9999)
-			else:
-				print('Authentication Failed')
+    def authenticate(self, **kwargs):
+        nMethods = struct.unpack('b', self.rsocket.recv(1))[0]
+        methods = struct.unpack('{}b'.format(nMethods),
+                                self.rsocket.recv(nMethods))
+        print('Server Supported Authentication Methods: ', methods)
+        if not kwargs['method'] in methods:
+            raise Exception('Unsupported authentication method')
+        if kwargs['method'] == 1:  #username, password authentication
+            try:
+                username = kwargs['username']
+                password = kwargs['password']
+            except:
+                print('Missing username and/or password')
+            self.rsocket.send(struct.pack('b', kwargs['method']))
+            usernameHash = hl.md5(username.encode()).digest()
+            passwordHash = hl.md5(password.encode()).digest()
+            self.rsocket.send(usernameHash + passwordHash)
+            authResponse = self.rsocket.recv(1024)
+            if authResponse == b'Authentication Successful':
+                self.listenLocal(9999)
+            else:
+                print('Authentication Failed')
 
-	def listenLocal(self, port = 9999):
-		Handler = HTTPHandler
-		httpd = socketserver.TCPServer(("", port), Handler)
-		print("Local Proxy Started at Port ", port)
-		httpd.serve_forever()
+    def listenLocal(self, port=9999):
+        Handler = HTTPHandler
+        socketserver.TCPServer.allow_reuse_address = True
+        httpd = socketserver.TCPServer(("", port), Handler)
+        print("Local Proxy Started at Port ", port)
+        httpd.serve_forever()
+
 
 d = Connection('127.0.0.1', 8080)
 #d.authenticate(method = 1, username = 'testUser', password = 'defaultpasswd')
